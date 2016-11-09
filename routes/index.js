d81acc6c02e527db32c6b47d7a1378aa66baa65f
shumana <shumana.chowdhury186@gmail.com>
2016-11-09 17:15:50 +0600

    signup completed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var Product = require('../models/product');
-var User = require('../models/user');
 var csrf = require('csurf');
 var passport = require('passport');
+
+var Product = require('../models/product');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
@@ -31,8 +31,8 @@ router.get('/user/signup', function(req, res, next){
 });
 
 router.post('/user/signup', passport.authenticate('local.signup', {
-    successRedirect: 'user/profile',
-    failureRedirect: 'user/signup',
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signup',
     failureFlash: true
 }));
 router.get('/user/profile', function(req, res, next){
