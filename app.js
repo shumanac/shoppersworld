e11dadc3e64aeba26ecca5a7cc73a9e4fd1b857a
shumana <shumana.chowdhury186@gmail.com>
2016-11-10 14:24:37 +0600

    signup validation

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var index = require('./routes/index');
+
 //var users = require('./routes/users');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
@@ -13,6 +13,8 @@ var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
 
+var routes = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 var app = express();
 mongoose.connect('localhost:27017/shopping');
@@ -41,8 +43,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-//app.use('/users', users);
+app.use(function(req, res, next){
+    res.locals.login = req.isAuthenticated();
+    next();
+});
+
+
+app.use('/user', userRoutes);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
