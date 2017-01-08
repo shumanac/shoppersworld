a62c48abd74a119486b1972c2f18ec216789dfbd
M Rezaur Rahman <mrezaurrahman@live.com>
2017-01-08 09:09:42 +0600

    conf change

--- config/env/production.js
+++ config/env/production.js
@@ -1,4 +1,5 @@
 module.exports = {
     port: process.env.PORT,
-    db: 'mongodb://heroku_jb2vkp2x:unanpb4k5sutkvb7ougqdqn798@ds157288.mlab.com:57288/heroku_jb2vkp2x'
+    db: "mongodb://heroku_jb2vkp2x:unanpb4k5sutkvb7ougqdqn798@ds157288.mlab.com:57288/heroku_jb2vkp2x"
+
 };
